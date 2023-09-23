8aa19416bef448cf9151bb6d7eb65cb8adceb7ea
eliasgermoliato <eliasgermoliato@hotmail.com>
2023-09-23 14:18:59 -0300

    refactor: adding the exercise description

--- exercises/week-1/ex-1.c
+++ exercises/week-1/ex-1.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 
+/* Exercise description: 
+
+Write a program that reads the number of students in a room.
+As output, the program must present the number of students and the total number of students in the room.
+
+For example, for the following INPUTS:
+
+10
+6
+
+The algorithm should print (OUTPUTS):
+
+Male Students: 10
+Female Students: 6
+Total: 16 */
+
 int main()
 {
     int number_male_students = 0, number_female_students = 0, total_students = 0;
 
-    printf("Digite a quantidade de alunos: \n");
+    printf("Enter the number of male students: \n");
     scanf("%d", &number_male_students);
 
-    printf("Digite a quantidade de alunas: \n");
+    printf("Enter the number of female students: \n");
     scanf("%d", &number_female_students);
 
     total_students = number_male_students + number_female_students;
 
-    printf("Alunos: %d\n", number_male_students);
-    printf("Alunas: %d\n", number_female_students);
+    printf("Male students: %d\n", number_male_students);
+    printf("Female students: %d\n", number_female_students);
     printf("Total: %d", total_students);
 
     return 0;
--- exercises/week-1/ex-2.c
+++ exercises/week-1/ex-2.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 
+/* Write a program that reads (via keyboard) in the following order, the number of Sony, Dell and Toshiba brand notebooks in a store. 
+As output, the program must present the number of notebooks broken down by brand (Sony, Dell and Toshiba) and their total.
+
+For example, for the following INPUTS:
+
+10
+6
+1
+
+The algorithm should print (OUTPUT) on the screen:
+
+10 Sony notebook(s)
+6 Dell notebook(s)
+1 Toshiba notebook(s)
+Total of 17 notebooks */
+
 int main()
 {
     int sony_notebooks = 0, dell_notebooks = 0, toshiba_notebooks = 0, total_notebooks = 0;
 
-    printf("Digite a quantidade de notebooks da Sony: \n");
+    printf("Enter the number of Sony notebooks: \n");
     scanf("%d", &sony_notebooks);
 
-    printf("Digite a quantidade de notebooks da Dell: \n");
+    printf("Enter the number of Dell notebooks: \n");
     scanf("%d", &dell_notebooks);
 
-    printf("Digite a quantidade de notebooks da Toshiba: \n");
+    printf("Enter the number of Toshiba notebooks: \n");
     scanf("%d", &toshiba_notebooks);
 
     total_notebooks = sony_notebooks + dell_notebooks + toshiba_notebooks;
 
-    printf("%d notebook(s) da Sony\n", sony_notebooks);
-    printf("%d notebook(s) da Dell\n", dell_notebooks);
-    printf("%d notebook(s) da Toshiba\n", toshiba_notebooks);
-    printf("Total de %d notebooks", total_notebooks);
+    printf("%d Sony notebook(s)\n", sony_notebooks);
+    printf("%d Dell notebook(s)\n", dell_notebooks);
+    printf("%d Toshiba notebooks(s)\n", toshiba_notebooks);
+    printf("Total of %d notebooks", total_notebooks);
 
     return 0;
 }
